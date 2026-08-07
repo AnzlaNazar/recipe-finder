3f98086864918136f1123973a908d7ff2084eab1
AnzlaNazar <anzlanazar181@gmail.com>
2026-08-08 00:58:27 +0500

    Stage 8: Favourites MVVM structure + Firebase: FavouritesModel

--- src/pages/Favourites/FavouritesModel.ts
+++ src/pages/Favourites/FavouritesModel.ts
@@ -1,3 +1,14 @@
-export function getFavouriteMeals() {
-  return []
+import type { Meal } from '../../types/meal'
+import { addFavourite, getFavourites, removeFavourite } from '../../services/firebaseService'
+
+export async function loadFavourites(): Promise<Meal[]> {
+  return getFavourites()
+}
+
+export async function saveFavourite(meal: Meal): Promise<void> {
+  await addFavourite(meal)
+}
+
+export async function deleteFavourite(idMeal: string): Promise<void> {
+  await removeFavourite(idMeal)
 }
